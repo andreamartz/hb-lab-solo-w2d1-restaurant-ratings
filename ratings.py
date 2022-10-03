c71a425427d282287c0ed8241824c466322898e0
Andrea Martz <14165788+andreamartz@users.noreply.github.com>
2022-10-03 15:14:56 -0500

    Write starter pseudocode

--- ratings.py
+++ ratings.py
@@ -1,4 +1,12 @@
 """Restaurant rating lister."""
 
-
-# put your code here
+# write a function that will accomplish these steps:
+# create a file object by opening a file
+# create an empty dictionary
+# loop through each line of the file:
+    # strip the line of white space on either side
+    # split the line on spaces to create a list of words
+    # save the key-value pairs into a dictionary, where the keys are the 
+        # restaurant names, and the values are the ratings
+# loop through the items in the dictionary:
+    # print a formatted string (fstring) for each item
